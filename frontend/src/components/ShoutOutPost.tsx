ca2209ada7ed1b3c1363451b150937a090d8c2bf
luykl <leah.logsin@gmail.com>
2021-06-04 17:07:11 -0400

    turn off likes

--- frontend/src/components/ShoutOutPost.tsx
+++ frontend/src/components/ShoutOutPost.tsx
@@ -1,5 +1,5 @@
 import ShoutOut from "../model/ShoutOut";
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext } from "react";
 import { AuthContext } from "../context/auth-context";
 // import { likeShoutOut } from "../service/ShoutOutApiService";
 
@@ -9,7 +9,7 @@ interface Props {
 
 function ShoutOutPost({ shoutOut }: Props) {
 
-    const [ liked, setLiked ] = useState(false);
+    // const [ liked, setLiked ] = useState(false);
     const { user } = useContext(AuthContext);
     
 
@@ -27,7 +27,9 @@ function ShoutOutPost({ shoutOut }: Props) {
             <h2>Shout out to {shoutOut.to}</h2>
             <h3> from {shoutOut.from}</h3>
             <p>{shoutOut.message}</p>
-            { user && <button onClick={()=>setLiked(true)}>like</button>}
+            { user && <button 
+            // onClick={()=>setLiked(true)}
+            >like</button>}
             {shoutOut.likes && <p>{shoutOut.likes.length} likes</p>}
 
         </div>
